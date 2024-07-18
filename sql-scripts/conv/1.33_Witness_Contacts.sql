c64e74bb42a2da1910fd31f2374886d1b7183b5a
dylangetssmart <dylans@smartadvocate.com>
2024-07-18 13:40:30 -0400

    Investigations > Witness, close #11

--- sql-scripts/conv/1.33_Witness_Contacts.sql
+++ sql-scripts/conv/1.33_Witness_Contacts.sql
@@ -1,10 +1,22 @@
--- USE SANeedlesKMY
+USE SANeedlesSLF
+GO
+
+-- Create individual contacts for Witness_1, Witness_2, and Witness_3 from user_party_data
+
+
+-- Create saga_ref field for another way to find these contacts
+if not exists (SELECT * FROM sys.columns WHERE Name = N'saga_ref' AND Object_ID = Object_ID(N'sma_MST_IndvContacts'))
+begin
+    ALTER TABLE [sma_MST_IndvContacts] ADD [saga_ref] [varchar](100) NULL; 
+end
 GO
 
 --
 ALTER TABLE [sma_MST_IndvContacts] DISABLE TRIGGER ALL
 GO
---
+
+
+-- Witness 1
 INSERT INTO [sma_MST_IndvContacts]
 (
      [cinbPrimary]
@@ -56,15 +68,16 @@ INSERT INTO [sma_MST_IndvContacts]
     ,[saga]
     ,[cinsSpouse]
     ,[cinsGrade]
+    ,[saga_ref]
 ) 
 SELECT DISTINCT 
      1                      as [cinbPrimary]
     ,10                     as [cinnContactTypeID]
     ,null                   as [cinnContactSubCtgID]
     ,''                     as [cinsPrefix]
-    ,dbo.get_firstword(utd.Witness_Name)     as [cinsFirstName]
+    ,dbo.get_firstword(upd.Witness_1)     as [cinsFirstName]
     ,''                               as [cinsMiddleName]
-    ,dbo.get_lastword(utd.Witness_Name)      as [cinsLastName]
+    ,dbo.get_lastword(upd.Witness_1)      as [cinsLastName]
     ,null                   as [cinsSuffix]
     ,null                   as [cinsNickName]
     ,1                      as [cinbStatus]
@@ -104,32 +117,230 @@ SELECT DISTINCT
     ,null                   as [cinsDeathPlace]
     ,''                     as [cinsMaidenName]
     ,''                     as [cinsOccupation]
-    ,''                     as [saga]
+    ,upd.case_id            as [saga]               -- case id to find later
     ,''                     as [cinsSpouse]
     ,-1                     as [cinsGrade]
-FROM [NeedlesKMY].[dbo].[user_tab_data] utd
-	--JOIN sma_trn_Cases cas
-		--on cas.cassCaseNumber = convert(varchar,ud.case_id)
-	
-	-- Link to SA Contact Card via:
-	-- user_tab_data -> user_tab_name -> names -> IndvOrgContacts_Indexed
-	join NeedlesKMY.dbo.user_tab_name utn
-		on utd.tab_id = utn.tab_id
-	join NeedlesKMY.dbo.names n
-		on utn.user_name = n.names_id
-	
-	-- Indv
-	--left join SANeedlesKMY.dbo.IndvOrgContacts_Indexed ioci
-		--on n.names_id = ioci.saga
-		--and ioci.CTG = 1
+    ,'witness'              as [saga_ref]
+FROM [NeedlesSLF].[dbo].[user_party_data] upd    
+WHERE isnull(upd.Witness_1,'') <> ''
+GO
 
-	-- Org
---	left join SANeedlesKMY.dbo.IndvOrgContacts_Indexed ioco
-	--	on n.names_id = ioco.saga
-		--and ioco.CTG = 2
+-- Witness 2
+INSERT INTO [sma_MST_IndvContacts]
+(
+     [cinbPrimary]
+    ,[cinnContactTypeID]
+    ,[cinnContactSubCtgID]
+    ,[cinsPrefix]
+    ,[cinsFirstName]
+    ,[cinsMiddleName]
+    ,[cinsLastName]
+    ,[cinsSuffix]
+    ,[cinsNickName]
+    ,[cinbStatus]
+    ,[cinsSSNNo]
+    ,[cindBirthDate]
+    ,[cinsComments]
+    ,[cinnContactCtg]
+    ,[cinnRefByCtgID]
+    ,[cinnReferredBy]
+    ,[cindDateOfDeath]
+    ,[cinsCVLink]
+    ,[cinnMaritalStatusID]
+    ,[cinnGender]
+    ,[cinsBirthPlace]
+    ,[cinnCountyID]
+    ,[cinsCountyOfResidence]
+    ,[cinbFlagForPhoto]
+    ,[cinsPrimaryContactNo]
+    ,[cinsHomePhone]
+    ,[cinsWorkPhone]
+    ,[cinsMobile]
+    ,[cinbPreventMailing]
+    ,[cinnRecUserID]
+    ,[cindDtCreated]
+    ,[cinnModifyUserID]
+    ,[cindDtModified]
+    ,[cinnLevelNo]
+    ,[cinsPrimaryLanguage]
+    ,[cinsOtherLanguage]
+    ,[cinbDeathFlag]
+    ,[cinsCitizenship]
+    ,[cinsHeight]
+    ,[cinnWeight]
+    ,[cinsReligion]
+    ,[cindMarriageDate]
+    ,[cinsMarriageLoc]
+    ,[cinsDeathPlace]
+    ,[cinsMaidenName]
+    ,[cinsOccupation]
+    ,[saga]
+    ,[cinsSpouse]
+    ,[cinsGrade]
+    ,[saga_ref]
+) 
+SELECT DISTINCT 
+     1                      as [cinbPrimary]
+    ,10                     as [cinnContactTypeID]
+    ,null                   as [cinnContactSubCtgID]
+    ,''                     as [cinsPrefix]
+    ,dbo.get_firstword(upd.Witness_2)     as [cinsFirstName]
+    ,''                               as [cinsMiddleName]
+    ,dbo.get_lastword(upd.Witness_2)      as [cinsLastName]
+    ,null                   as [cinsSuffix]
+    ,null                   as [cinsNickName]
+    ,1                      as [cinbStatus]
+    ,null                   as [cinsSSNNo]
+    ,null                   as [cindBirthDate]
+    ,null                   as [cinsComments]
+    ,1                      as [cinnContactCtg]
+    ,''                     as [cinnRefByCtgID]
+    ,''                     as [cinnReferredBy]
+    ,null                   as [cindDateOfDeath]
+    ,''                     as [cinsCVLink]
+    ,''                     as [cinnMaritalStatusID]
+    ,1                      as [cinnGender]
+    ,''                     as [cinsBirthPlace]
+    ,1                      as [cinnCountyID]
+    ,1                      as [cinsCountyOfResidence]
+    ,null                   as [cinbFlagForPhoto]
+    ,null                   as [cinsPrimaryContactNo]
+    ,''                     as [cinsHomePhone]
+    ,''                     as [cinsWorkPhone]
+    ,null                   as [cinsMobile]
+    ,0                      as [cinbPreventMailing]
+    ,368                    as [cinnRecUserID]
+    ,GETDATE()              as [cindDtCreated]
+    ,''                     as [cinnModifyUserID]
+    ,null                   as [cindDtModified]
+    ,0                      as [cinnLevelNo]
+    ,''                     as [cinsPrimaryLanguage]
+    ,''                     as [cinsOtherLanguage]
+    ,''                     as [cinbDeathFlag]
+    ,''                     as [cinsCitizenship]
+    ,null + null            as [cinsHeight]
+    ,null                   as [cinnWeight]
+    ,''                     as [cinsReligion]
+    ,null                   as [cindMarriageDate]
+    ,null                   as [cinsMarriageLoc]
+    ,null                   as [cinsDeathPlace]
+    ,''                     as [cinsMaidenName]
+    ,''                     as [cinsOccupation]
+    ,upd.case_id            as [saga]               -- case id to find later
+    ,''                     as [cinsSpouse]
+    ,-1                     as [cinsGrade]
+    ,'witness'              as [saga_ref]
+FROM [NeedlesSLF].[dbo].[user_party_data] upd
+WHERE isnull(upd.Witness_2,'') <> ''
+GO
 
-WHERE isnull(utd.Witness_Name,'')<>''
-
+-- Witness 3
+INSERT INTO [sma_MST_IndvContacts]
+(
+     [cinbPrimary]
+    ,[cinnContactTypeID]
+    ,[cinnContactSubCtgID]
+    ,[cinsPrefix]
+    ,[cinsFirstName]
+    ,[cinsMiddleName]
+    ,[cinsLastName]
+    ,[cinsSuffix]
+    ,[cinsNickName]
+    ,[cinbStatus]
+    ,[cinsSSNNo]
+    ,[cindBirthDate]
+    ,[cinsComments]
+    ,[cinnContactCtg]
+    ,[cinnRefByCtgID]
+    ,[cinnReferredBy]
+    ,[cindDateOfDeath]
+    ,[cinsCVLink]
+    ,[cinnMaritalStatusID]
+    ,[cinnGender]
+    ,[cinsBirthPlace]
+    ,[cinnCountyID]
+    ,[cinsCountyOfResidence]
+    ,[cinbFlagForPhoto]
+    ,[cinsPrimaryContactNo]
+    ,[cinsHomePhone]
+    ,[cinsWorkPhone]
+    ,[cinsMobile]
+    ,[cinbPreventMailing]
+    ,[cinnRecUserID]
+    ,[cindDtCreated]
+    ,[cinnModifyUserID]
+    ,[cindDtModified]
+    ,[cinnLevelNo]
+    ,[cinsPrimaryLanguage]
+    ,[cinsOtherLanguage]
+    ,[cinbDeathFlag]
+    ,[cinsCitizenship]
+    ,[cinsHeight]
+    ,[cinnWeight]
+    ,[cinsReligion]
+    ,[cindMarriageDate]
+    ,[cinsMarriageLoc]
+    ,[cinsDeathPlace]
+    ,[cinsMaidenName]
+    ,[cinsOccupation]
+    ,[saga]
+    ,[cinsSpouse]
+    ,[cinsGrade]
+    ,[saga_ref]
+) 
+SELECT DISTINCT 
+     1                      as [cinbPrimary]
+    ,10                     as [cinnContactTypeID]
+    ,null                   as [cinnContactSubCtgID]
+    ,''                     as [cinsPrefix]
+    ,dbo.get_firstword(upd.Witness_3)     as [cinsFirstName]
+    ,''                               as [cinsMiddleName]
+    ,dbo.get_lastword(upd.Witness_3)      as [cinsLastName]
+    ,null                   as [cinsSuffix]
+    ,null                   as [cinsNickName]
+    ,1                      as [cinbStatus]
+    ,null                   as [cinsSSNNo]
+    ,null                   as [cindBirthDate]
+    ,null                   as [cinsComments]
+    ,1                      as [cinnContactCtg]
+    ,''                     as [cinnRefByCtgID]
+    ,''                     as [cinnReferredBy]
+    ,null                   as [cindDateOfDeath]
+    ,''                     as [cinsCVLink]
+    ,''                     as [cinnMaritalStatusID]
+    ,1                      as [cinnGender]
+    ,''                     as [cinsBirthPlace]
+    ,1                      as [cinnCountyID]
+    ,1                      as [cinsCountyOfResidence]
+    ,null                   as [cinbFlagForPhoto]
+    ,null                   as [cinsPrimaryContactNo]
+    ,''                     as [cinsHomePhone]
+    ,''                     as [cinsWorkPhone]
+    ,null                   as [cinsMobile]
+    ,0                      as [cinbPreventMailing]
+    ,368                    as [cinnRecUserID]
+    ,GETDATE()              as [cindDtCreated]
+    ,''                     as [cinnModifyUserID]
+    ,null                   as [cindDtModified]
+    ,0                      as [cinnLevelNo]
+    ,''                     as [cinsPrimaryLanguage]
+    ,''                     as [cinsOtherLanguage]
+    ,''                     as [cinbDeathFlag]
+    ,''                     as [cinsCitizenship]
+    ,null + null            as [cinsHeight]
+    ,null                   as [cinnWeight]
+    ,''                     as [cinsReligion]
+    ,null                   as [cindMarriageDate]
+    ,null                   as [cinsMarriageLoc]
+    ,null                   as [cinsDeathPlace]
+    ,''                     as [cinsMaidenName]
+    ,''                     as [cinsOccupation]
+    ,upd.case_id            as [saga]               -- case id to find later
+    ,''                     as [cinsSpouse]
+    ,-1                     as [cinsGrade]
+    ,'witness'              as [saga_ref]
+FROM [NeedlesSLF].[dbo].[user_party_data] upd
+WHERE isnull(upd.Witness_3,'') <> ''
 GO
 
 --
