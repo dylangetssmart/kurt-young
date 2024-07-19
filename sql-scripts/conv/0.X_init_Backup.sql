55a40ca89408385394dbddde12c5d5ee9f2a04d3
dylangetssmart <dylans@smartadvocate.com>
2024-07-19 12:22:14 -0400

    update backup scripts (hardcode)

--- sql-scripts/conv/0.X_init_Backup.sql
+++ sql-scripts/conv/0.X_init_Backup.sql
@@ -3,9 +3,9 @@ DECLARE @currentTimestamp NVARCHAR(20);
 SET @currentTimestamp = FORMAT(GETDATE(), 'yyyy-MM-dd_HH-mm');
 DECLARE @prefix NVARCHAR(10) = '0.X_';
 DECLARE @step NVARCHAR(10) = 'Init';
-DECLARE @databaseName1 sysname = 'NeedlesKMY';
-DECLARE @databaseName2 sysname = 'SANeedlesKMY';
-DECLARE @directory NVARCHAR(128) = 'C:\LocalConv\NeedlesKMY\backups\';
+DECLARE @databaseName1 sysname = 'NeedlesSLF';
+DECLARE @databaseName2 sysname = 'SANeedlesSLF';
+DECLARE @directory NVARCHAR(128) = 'C:\LocalConv\NeedlesSLF\backups\';
 
 -- Specify the backup file paths with timestamp
 DECLARE @backupPath1 NVARCHAR(255) = @directory + @prefix + @databaseName1 + '_After' + @step + '_' + @currentTimestamp + '.bak';
