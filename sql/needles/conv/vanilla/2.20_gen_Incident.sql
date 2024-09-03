eb4ba6afcaf2ed8ca2d1abff404e668fe061169c
dylangetssmart <dylans@smartadvocate.com>
2024-09-03 09:51:49 -0400

    Bugfix: replace synopsis, add StateCode variable

--- sql/needles/conv/vanilla/2.20_gen_Incident.sql
+++ sql/needles/conv/vanilla/2.20_gen_Incident.sql
@@ -1,5 +1,5 @@
 -- use [TestNeedles]
-GO
+-- GO
 
 /*
 alter table [sma_TRN_Incidents] disable trigger all
@@ -8,6 +8,7 @@ DBCC CHECKIDENT ('[sma_TRN_Incidents]', RESEED, 0);
 alter table [sma_TRN_Incidents] enable trigger all
 */
 
+DECLARE @StateCode NVARCHAR(2) = 'VA'
 
 ---
 ALTER TABLE [sma_TRN_Incidents] DISABLE TRIGGER ALL
@@ -17,56 +18,74 @@ GO
 ---
 INSERT INTO [sma_TRN_Incidents]
 (
-       [CaseId]
-      ,[IncidentDate]
-      ,[StateID]
-      ,[LiabilityCodeId]
-      ,[IncidentFacts]
-      ,[MergedFacts]
-      ,[Comments]
-      ,[IncidentTime]
-      ,[RecUserID]
-      ,[DtCreated]
-      ,[ModifyUserID]
-      ,[DtModified]
+	[CaseId]
+	,[IncidentDate]
+	,[StateID]
+	,[LiabilityCodeId]
+	,[IncidentFacts]
+	,[MergedFacts]
+	,[Comments]
+	,[IncidentTime]
+	,[RecUserID]
+	,[DtCreated]
+	,[ModifyUserID]
+	,[DtModified]
 )
-SELECT 
-	CAS.casnCaseID				as CaseId
-	,case
-		when ( C.[date_of_incident] between '1900-01-01' and '2079-06-06' ) 
-			then convert(date,C.[date_of_incident]) 
-		else null 
-		end 					as IncidentDate
-	,(
-		select sttnStateID
-		from sma_MST_States
-		where sttsCode='VA'
-	)							as [StateID]
-	,0							as LiabilityCodeId
-	,null						as IncidentFacts
-	,null						as [MergedFacts]
-	,null						as [Comments]
-	,u.Time_of_Accident			as [IncidentTime]
-	,368						as [RecUserID]
-	,getdate()					as [DtCreated]
-	,null						as [ModifyUserID]
-	,null						as [DtModified]
-FROM TestNeedles.[dbo].[cases_Indexed] C
+	SELECT 
+		CAS.casnCaseID				as CaseId
+		,case
+			when (C.[date_of_incident] between '1900-01-01' and '2079-06-06')
+				then convert(date,C.[date_of_incident]) 
+			else null 
+		end							as IncidentDate
+		,case
+			when exists (
+							select * from
+							sma_MST_States where
+							sttsCode = U.[State]
+						)
+			 	then (
+						select sttnStateID
+						from sma_MST_States
+						where sttsCode = U.[State]
+					)
+			else (
+					select sttnStateID
+					from sma_MST_States
+					where sttsCode = @StateCode
+				)
+		end							as [StateID]
+		-- ,(
+		-- 	select sttnStateID
+		-- 	from sma_MST_States
+		-- 	where sttsCode='VA'
+		-- )							as [StateID]
+		,0							as LiabilityCodeId
+		,C.synopsis + char(13) +
+		--isnull('Description of Accident:' + nullif(u.Description_of_Accident,'') + CHAR(13),'') + 
+		''							as IncidentFacts
+		,''							as [MergedFacts]
+		,null						as [Comments]
+		,u.Time_of_Accident			as [IncidentTime]
+		,368						as [RecUserID]
+		,getdate()					as [DtCreated]
+		,null						as [ModifyUserID]
+		,null						as [DtModified]
+	FROM TestNeedles.[dbo].[cases_Indexed] C
 	JOIN TestNeedles.[dbo].[user_case_data] U
-		on U.casenum=C.casenum
+		on U.casenum = C.casenum
 	JOIN [sma_TRN_cases] CAS
 		on CAS.cassCaseNumber = convert(varchar,C.casenum)
-where ISNULL(u.Time_of_Accident, '') <> ''
-                
-
 
 UPDATE CAS
-SET CAS.casdIncidentDate=INC.IncidentDate,
-    CAS.casnStateID=INC.StateID,
-    CAS.casnState=INC.StateID
+SET
+	CAS.casdIncidentDate = INC.IncidentDate
+	,CAS.casnStateID = INC.StateID
+	,CAS.casnState = INC.StateID
 FROM sma_trn_cases as CAS
-LEFT JOIN sma_TRN_Incidents as INC on casnCaseID=caseid
-WHERE INC.CaseId=CAS.casncaseid 
+LEFT JOIN sma_TRN_Incidents as INC
+	on casnCaseID = caseid
+WHERE INC.CaseId = CAS.casncaseid 
 
 ---
 ALTER TABLE [sma_TRN_Incidents] ENABLE TRIGGER ALL
