0a35e01c1a2e78e80d078142d95e7703b7483cd4
dylangetssmart <dylans@smartadvocate.com>
2024-10-30 15:42:57 -0400

    updates

--- sql/needles/conv/0.30_ImplementationUsers.sql
+++ sql/needles/conv/0.30_ImplementationUsers.sql
@@ -1,45 +1,120 @@
--- USE TestNeedles
+/*
+This script manages the population of the `implementation_users` table for the JoelBieberSA_Needles project.
+It consists of two main phases, controlled by the `@Phase` variable:
 
-if exists (select * from sys.objects where name='implementation_users' and type='U')
-begin
-    drop table implementation_users
-end
+    - Phase 1: Initial Conversion
+      - Seeds the `implementation_users` table from the `JoelBieberNeedles..staff` table.
+      - Used in the initial data conversion phase when only `staff` data is available.
+
+    - Phase 2: Implementation Database Seeding
+      - Drops and repopulates `implementation_users` based on the implementation database.
+      - Joins `sma_mst_users` and `sma_MST_IndvContacts` from the implementation database.
+      - Adds `staff_code` from `JoelBieberNeedles..staff` if available.
+      - Used in later project phases when the implementation database is the primary source.
+
+Usage:
+- Set the `@Phase` variable to `1` for Phase 1 or `2` for Phase 2, then run the script.
+
+Requirements:
+- Phase 1 assumes `staff` records exist in `JoelBieberNeedles..staff`.
+- Phase 2 assumes that both `sma_mst_users` and `sma_MST_IndvContacts` are populated in the implementation database.
+
+*/
+
+USE JoelBieberSA_Needles;
+
+IF OBJECT_ID('implementation_users', 'U') IS NOT NULL
+BEGIN
+	DROP TABLE implementation_users;
+END;
+
+-- Create the implementation_users table
+CREATE TABLE implementation_users (
+	SAContactID NVARCHAR(25)
+   ,SAUserID NVARCHAR(25)
+   ,StaffCode NVARCHAR(50)
+   ,full_name NVARCHAR(100)
+   ,SALoginID NVARCHAR(50)
+   ,Prefix NVARCHAR(10)
+   ,SAFirst NVARCHAR(50)
+   ,SAMiddle VARCHAR(5)
+   ,SALast NVARCHAR(50)
+   ,Suffix NVARCHAR(10)
+   ,Active BIT
+   ,Visible BIT
+);
 GO
 
-CREATE TABLE implementation_users
-(
-    StaffCode varchar(50)
-    ,SAloginID varchar(20)
-    ,Prefix varchar(10)
-    ,SAFirst varchar(50)
-    ,SAMiddle varchar(5)
-    ,SALast varchar(50)
-    ,suffix varchar(15)
-    ,Active varchar(1)
-    ,visible varchar(1)
-)
-GO
+-- Declare a variable to control the execution phase
+DECLARE @Phase INT = 1;  -- Set to 1 for Phase 1, 2 for Phase 2
 
--- INSERT INTO implementation_users (StaffCode, SAloginID, Prefix, SAFirst, SAMiddle, SALast, suffix, Active, Visible)
+IF @Phase = 1
+BEGIN
+	-- Phase 1: Initial Conversion - Seed from JoelBieberNeedles..staff
 
--- ds 2024-05-31 // Modified to insert data into the implementation_users table from the dbo.staff table
-INSERT INTO implementation_users
-(
-    StaffCode
-    ,SAloginID
-    ,Prefix
-    ,SAFirst
-    ,SAMiddle
-    ,SALast
-    ,suffix
-)
-SELECT 
-    staff_code                          as StaffCode
-    ,staff_code                         as SAloginID
-	,prefix                             as Prefix
-	,dbo.get_firstword(s.full_name)     as SAFirst
-    ,''                                 as SAMiddle
-    ,dbo.get_lastword(s.full_name)      as SALast
-    ,suffix                             as suffix
-FROM [TestNeedles].[dbo].[staff] s
-GO
+	INSERT INTO implementation_users
+		(
+		SAContactID
+	   ,SAUserID
+	   ,StaffCode
+	   ,full_name
+	   ,SALoginID
+	   ,Prefix
+	   ,SAFirst
+	   ,SAMiddle
+	   ,SALast
+	   ,Suffix
+		)
+		SELECT
+			NULL						   AS SAContactID
+		   ,NULL						   AS SAUserID
+		   ,staff_code					   AS StaffCode
+		   ,s.full_name					   AS full_name
+		   ,staff_code					   AS SAloginID
+		   ,prefix						   AS Prefix
+		   ,dbo.get_firstword(s.full_name) AS SAFirst
+		   ,''							   AS SAMiddle
+		   ,dbo.get_lastword(s.full_name)  AS SALast
+		   ,suffix						   AS Suffix
+		FROM [JoelBieberNeedles].[dbo].[staff] s;
+END
+ELSE
+IF @Phase = 2
+BEGIN
+	-- Phase 2: Use implementation database as starting point and add staff_code from JoelBieberNeedles..staff
+
+	INSERT INTO implementation_users
+		(
+		SAContactID
+	   ,SAUserID
+	   ,StaffCode
+	   ,full_name
+	   ,SALoginID
+	   ,Prefix
+	   ,SAFirst
+	   ,SAMiddle
+	   ,SALast
+	   ,Suffix
+	   ,Active
+	   ,Visible
+		)
+		SELECT
+			smic.cinnContactID
+		   ,u.usrnUserID
+		   ,COALESCE(s.staff_code, '') AS StaffCode
+		   ,s.full_name
+		   ,u.usrsLoginID			   AS SALoginID
+		   ,smic.cinsPrefix			   AS Prefix
+		   ,smic.cinsFirstName		   AS SAFirst
+		   ,smic.cinsMiddleName		   AS SAMddle
+		   ,smic.cinsLastName		   AS SALast
+		   ,smic.cinsSuffix			   AS Suffix
+		   ,u.usrbActiveState		   AS Active
+		   ,u.usrbIsShowInSystem	   AS Visible
+		--select * 
+		FROM [JoelBieber_Imp_2024-10-28]..sma_mst_users u
+		JOIN [JoelBieber_Imp_2024-10-28]..sma_MST_IndvContacts smic
+			ON smic.cinnContactID = u.usrnContactID
+		LEFT JOIN JoelBieberNeedles..staff s
+			ON s.full_name = smic.cinsFirstName + ' ' + smic.cinsLastName
+END;
